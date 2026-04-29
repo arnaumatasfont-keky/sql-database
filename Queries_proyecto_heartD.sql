eb9b269a8a0ab8d82f00c8fd8e4d856482b23148
arnaumatasfont-keky <arnaumatasfont@gmail.com>
2026-04-29 18:02:44 +0200

    Queries
    Se han importado los datos correctamente. Actualmente trabajando en queries, de momento hechas, 2. Diagrama actualizado.

---
+++ Queries_proyecto_heartD.sql
@@ -0,0 +1,29 @@
+-- 1. Distribución general de la enfermedad --
+-- Insight: Entender cuantos pacientes tienen enfermedad vs los que no --
+USE heart_db;
+
+SELECT
+	CASE
+		WHEN hm.diagnosis_id = 0 THEN 'Ausencia de enfermedad cardiovascular'
+        ELSE 'Con enfermedad cardiovascular'
+	END AS Estado_salud_paciente,
+    COUNT(*) AS total_pacientes
+FROM heart_main AS hm
+JOIN dim_target AS dt ON hm.diagnosis_id = dt.diagnosis_id
+group by Estado_salud_paciente;
+
+
+-- 2. Edad promedio de la enfermedad cardiovascular -- 
+-- Comprobar la relación entre la edad y la enfermedad cardiovascular --
+
+SELECT 
+    CASE 
+        WHEN h.diagnosis_id = 0 THEN 'Sano' 
+        ELSE 'Con Enfermedad' 
+    END AS estado_salud,
+    ROUND(AVG(h.age), 1) AS edad_media,
+    MIN(h.age) AS edad_minima,
+    MAX(h.age) AS edad_maxima
+FROM heart_main h
+JOIN dim_target t ON h.diagnosis_id = t.diagnosis_id
+GROUP BY estado_salud;
