a6a7f83130650002203488172d77554d59ce678f
arnaumatasfont-keky <arnaumatasfont@gmail.com>
2026-04-30 12:58:10 +0200

    termino queries

--- Queries_proyecto_heartD.sql
+++ Queries_proyecto_heartD.sql
@@ -30,11 +30,48 @@ GROUP BY estado_salud;
 
 -- 3. Distribución por género
 -- Identificar diferencias en la enfermedad entre hombres y mujeres
+SELECT 
+    ds.sex_desc AS Genero,
+    CASE WHEN hm.diagnosis_id = 0 THEN 'Sano' ELSE 'Enfermo' END AS Estado,
+    COUNT(*) AS Total_Pacientes,
+    ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(PARTITION BY ds.sex_desc), 2) AS Porcentaje_por_Genero
+FROM heart_main AS hm
+JOIN dim_sex AS ds ON hm.sex_id = ds.sex_id
+GROUP BY ds.sex_desc, Estado;
 
-
--- 4. Tipo de chest pain vs enfermedad
+-- 4. Tipo de chest pain en pacientes enfermos
 -- Identificar diferencias en la enfermedad entre hombres y mujeres
 
+SELECT 
+    dc.cp_name AS Simptoma_Principal,
+    dt.diagnosis_desc AS Estado_enfermedad,
+    COUNT(*) AS Casos_Detectados,
+    CAST(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(PARTITION BY dt.diagnosis_desc) AS SIGNED) AS Porcentaje_total
+FROM heart_main AS hm
+JOIN dim_cp AS dc ON hm.cp_id = dc.cp_id
+JOIN dim_target AS dt ON hm.diagnosis_id = dt.diagnosis_id
+GROUP BY dc.cp_name, dt.diagnosis_desc, hm.diagnosis_id
+ORDER BY hm.diagnosis_id ASC, Porcentaje_total DESC;
 
--- 5. Tipos de chest pain más relevantes en pacientes con enfermedad
+-- 5. Relación entre niveles de colesterol y presencia de enfermedad y gravedad.
 -- filtrar solo los casos más significativos
+
+SELECT
+dt.diagnosis_desc,
+AVG(hm.chol) AS media_chol,
+COUNT(*) AS num_pacientes
+FROM heart_main AS hm
+JOIN dim_target AS dt ON hm.diagnosis_id = dt.diagnosis_id
+GROUP BY dt.diagnosis_id
+ORDER BY media_chol DESC;
+
+-- 6. Relación entre los niveles de presión arterial y presencia de enfermedad y gravedad.
+
+SELECT
+dt.diagnosis_desc,
+AVG(hm.trestbps) AS media_presion_arterial,
+COUNT(*) AS num_pacientes
+FROM heart_main AS hm
+JOIN dim_target AS dt ON hm.diagnosis_id = dt.diagnosis_id
+GROUP BY dt.diagnosis_id
+ORDER BY media_presion_arterial DESC;
